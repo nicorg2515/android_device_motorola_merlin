2a04d40d2ea8324922787d66a5034e5bfb1bef11
nico <nicorg2529@gmail.com>
2018-07-25 16:06:11 +0000

    Branch from aex to carbon

--- carbon_merlin.mk
+++ carbon_merlin.mk
@@ -18,9 +18,6 @@ $(call inherit-product, device/motorola/merlin/full_merlin.mk)
 # Bootanimation resolution.
 TARGET_BOOT_ANIMATION_RES := 720
 
-# Inherit some common aosp stuff.
-$(call inherit-product, vendor/aosp/common.mk)
-
 # Boot animation
 TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_SCREEN_WIDTH := 720
@@ -28,7 +25,7 @@ TARGET_SCREEN_HEIGHT := 1280
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := merlin
-PRODUCT_NAME := aosp_merlin
+PRODUCT_NAME := carbon_merlin
 PRODUCT_MODEL := MotoG3-TE
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
